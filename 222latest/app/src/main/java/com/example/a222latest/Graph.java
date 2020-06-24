95f8f70b8c19a073ce30d815c46029e63adab0f5
Ozan Yeşiller <50305026+rhasepy@users.noreply.github.com>
2020-06-24 22:43:03 +0300

    javadoc added

--- 222latest/app/src/main/java/com/example/a222latest/Graph.java
+++ 222latest/app/src/main/java/com/example/a222latest/Graph.java
@@ -4,15 +4,39 @@ import java.util.Iterator;
 
 public interface Graph
 {
+    /**
+     * @return vertex number
+     */
     int getNumV();
 
+    /**
+     * @return info of directed or not
+     */
     boolean isDirected();
 
+    /**
+     * insert method
+     * @param edge to be insert
+     */
     void insert(Edge edge);
 
+    /**
+     * @param source source of edge
+     * @param dest dest of edge
+     * @return is edge or not
+     */
     boolean isEdge(int source, int dest);
 
+    /**
+     * @param source source of edge
+     * @param dest dest of edge
+     * @return source -> dest edge
+     */
     Edge getEdge(int source, int dest);
 
+    /**
+     * @param source source of list
+     * @return iterator of source of list
+     */
     Iterator<Edge> edgeIterator(int source);
 }
