630e0c4a5c8da596ad5c4c24136412f869d44d54
fatihkaan22 <fatihkaan22@gmail.com>
2020-06-24 12:34:38 +0300

    posts on main menu

--- 222latest/app/src/main/java/com/example/a222latest/MainActivity.java
+++ 222latest/app/src/main/java/com/example/a222latest/MainActivity.java
@@ -11,21 +11,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     protected DrawerLayout drawerLayout;
     ActionBarDrawerToggle drawerToggle;
     Toolbar toolbar;
     NavigationView navigationView;
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drawer);
+
+        logIn();
+        firebaseAuth = FirebaseAuth.getInstance();
 
         drawerLayout = findViewById(R.id.drawer);
         toolbar = findViewById(R.id.toolbar_drawer);
@@ -41,6 +51,9 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         navigationView = findViewById(R.id.navigationView);
         navigationView.setNavigationItemSelectedListener(this);
 
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.content2, new HomeFragment()).commit();
+
     }
 
     @Override
@@ -51,7 +64,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 startActivity(new Intent(this, LoginMainActivity.class));
                 break;
             case R.id.posts:
-                startActivity(new Intent(this, ShowPostActivity.class));
+                drawerLayout.closeDrawers();
+//                startActivity(new Intent(this, MainActivity.class));
                 break;
             case R.id.newPost:
                 startActivity(new Intent(this, AddPostActivity.class));
@@ -73,5 +87,36 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 break;
         }
         return false;
+    }
+
+    private void logIn() {
+        String email = "deneme@gmail.com";
+        String password = "123456";
+
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                Toast.makeText(this, "Loggedin", Toast.LENGTH_SHORT).show();
+            } else
+                Toast.makeText(this, "error", Toast.LENGTH_SHORT).show();
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_post, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.menu.menu_post)
+            Toast.makeText(this, "error", Toast.LENGTH_SHORT).show();
+
+        return super.onOptionsItemSelected(item);
     }
 }
